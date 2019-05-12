39ad483ada7c83e03a080e316ff702ad35dd1105
zios07 <Zios1997>
2019-05-12 17:12:11 +0000

    permissions update

--- aged-care-web/src/app/components/patient/patient-list/patient-list.component.ts
+++ aged-care-web/src/app/components/patient/patient-list/patient-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PatientService } from 'src/app/services/patient.service';
 import { ToastrService } from 'ngx-toastr';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-patient-list',
@@ -11,10 +12,14 @@ import { ToastrService } from 'ngx-toastr';
 export class PatientListComponent implements OnInit {
   patients: Array<any>;
   fewpatients: Array<any>;
+  connectedRole;
 
   constructor(private patientService: PatientService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    const helper = new JwtHelperService();
+    const token = helper.decodeToken(localStorage.getItem('token'));
+    this.connectedRole = token.role;
     this.patientService.getAll().subscribe(data => {
       this.fewpatients = data;
       for (const patient of this.fewpatients) {
